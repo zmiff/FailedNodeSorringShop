c04439d3c00946b69500f66a8055b13004e3c592
Ugur Tamer <ugt@brunata.com>
2018-08-18 23:40:28 +0200

    chenged js to JS

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
-var middleware = require('./../js/middleware');
+var middleware = require('./../JS/middleware');
 
 var csrfProtection = csrf({cookie:true});
 router.use(csrfProtection);
